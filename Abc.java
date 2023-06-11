dde170ab8d79862662def3d35c828cb6ed03be51
aya-shiro <whitecheck00@gmail.com>
2023-06-11 09:17:40 +0000

    [addRandomHand]Abc

--- Abc.java
+++ Abc.java
@@ -23,6 +23,15 @@ public class Abc {
     System.out.println("Your name is " + name + ",right?");
     in.close();  //コンソールからの入力終了記述。あってもなくても挙動に変化なし
     
+    //一次元配列
+    int[] scores = {50, 55, 60, 105, 110};
+    // for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < scores.length; i++) {  //scores.lengthで配列の要素数に合わせて柔軟に対応
+      if (i % 2 == 0) {
+        System.out.println(scores[i]);
+        continue;  //continueで偶数スキップ
+      }
+    }
     
     double price = 120.0;
     int amount = 15;
@@ -34,10 +43,20 @@ public class Abc {
     } else {
       System.out.println("価格は合計" + totalPrice + "円です");
     }
+    
+    RandomHand();
   }
   
   public static double items(double price, int amount){
     double totalPrice = price * amount;
     return totalPrice;
   }
+  
+  // ランダム
+  public static void RandomHand() {
+    String[] janken = {"グー", "チョキ", "パー"};
+    int randomNumber = (int)(Math.random()*3);
+    System.out.println(janken[randomNumber]);
+  }
+  
 }
