5fa8eabe2b5b8c65073347d6254d6e42f734bbfd
aya-shiro <whitecheck00@gmail.com>
2023-07-02 12:40:04 +0000

    [add_HashMap]Ghi

--- Ghi.java
+++ Ghi.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Ghi {
   public static void main(String[] args) {
@@ -18,13 +19,14 @@ public class Ghi {
     }
     
     WrapTest.wrap();
-
+    HashMapTest.company();
   }
 }
 
 class WrapTest {
   public static void wrap() {
-    ArrayList<Integer> numbers = new ArrayList<>();
+    // 基本型のArrayListはラッパークラスを用いる
+    ArrayList<Integer> numbers = new ArrayList<>();  //2つ目のデータ型Stringは省略可
     numbers.add(Integer.valueOf(10));
     numbers.add(Integer.valueOf(20));
     numbers.add(Integer.valueOf(30));
@@ -35,6 +37,18 @@ class WrapTest {
     
     int num20 = numbers.get(1);  //オートボクシングを活用した書き方
     System.out.println(num20);
+  }
+}
 
+class HashMapTest {
+  public static void company() {
+    HashMap<String, String> worker = new HashMap<String, String>();
+    worker.put("name", "Kate");
+    worker.put("belong", "DRYstore");
+    System.out.println(worker.entrySet());  //キーと値をセットで取得する、各括弧で出力
+    System.out.println(worker.keySet());  //キーを一覧で取得
+    System.out.println(worker.values());  //値を一覧で取得
+    System.out.println(worker.get("name"));  //キー"name"に対応する値を出力
+    System.out.println(worker.get("belong"));  //キー"belong"に対応する値を出力
   }
 }
