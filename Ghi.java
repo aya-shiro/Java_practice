38de4501dda5d2b32e364dd425219af26b6b03f9
aya-shiro <whitecheck00@gmail.com>
2023-07-02 12:19:05 +0000

    [add_ArrayList2]Ghi

--- Ghi.java
+++ Ghi.java
@@ -16,5 +16,25 @@ public class Ghi {
     for (int i = 0; i < array.size(); i++ ) {
       System.out.println(array.get(i));
     }
+    
+    WrapTest.wrap();
+
+  }
+}
+
+class WrapTest {
+  public static void wrap() {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(Integer.valueOf(10));
+    numbers.add(Integer.valueOf(20));
+    numbers.add(Integer.valueOf(30));
+    
+    Integer number10 = numbers.get(0);
+    int num10 = number10.intValue();
+    System.out.println(num10);
+    
+    int num20 = numbers.get(1);  //オートボクシングを活用した書き方
+    System.out.println(num20);
+
   }
 }
